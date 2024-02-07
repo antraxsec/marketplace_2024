19c26cdfc158feaffae2dd113058825b541803f2
antraxsec <39973938+antraxsec@users.noreply.github.com>
2024-02-07 07:46:02 -0400

    hardy 07/02/24

--- src/app/pablito/page.js
+++ src/app/pablito/page.js
@@ -1,7 +1,29 @@
-// import api from "../api";
+import api from "../api";
 
-// export default async function page() {
+export default async function page() {
+    const productos = await api.productos();
+    console.log(productos);
+    return <div>page</div>;
+}
+
+
+
+
+
+//dunciona
+// import api from "../../api";
+
+// export async function getStaticPaths() {
 //   const productos = await api.productos();
-//   console.log(productos);
+//   const paths = productos.map((producto) => ({
+//     params: { id: producto.id_producto.toString() },
+//   }));
+//   return { paths, fallback: false };
+// }
+
+// export default async function page({ params: { id } }) {
+//   const producto = await api.producto(id);
+//   console.log("producto ", producto);
+//   console.log(id);
 //   return <div>page</div>;
 // }
